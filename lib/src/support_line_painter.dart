be2709313dbfc0703ab3515c298c3e253b4c0d3a
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-10-23 14:11:14 +0300

    Update colors

--- lib/src/support_line_painter.dart
+++ lib/src/support_line_painter.dart
@@ -6,17 +6,15 @@ class SupportLinePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final rect = Rect.fromLTWH(0, size.height / 2, size.width, 1);
-    final shader = const LinearGradient(
+    final shader = LinearGradient(
       colors: [
-        Color(0xFF111111),
-        Color(0xFFFFFFFF),
-        Color(0xFFFFFFFF),
-        Color(0xFF111111)
+        const Color(0xFF111111).withOpacity(.7),
+        const Color(0xFFFFFFFF).withOpacity(.7),
+        const Color(0xFFFFFFFF).withOpacity(.7),
+        const Color(0xFF111111).withOpacity(.7)
       ],
-      stops: [0, .1, .8, 1],
-    ).createShader(
-      Rect.fromPoints(Offset(0, size.height / 2), Offset(size.width, 1)),
-    );
+      stops: const [0, .1, .8, 1],
+    ).createShader(rect);
     final Paint paint = Paint()..shader = shader;
     canvas.drawRect(rect, paint);
   }
