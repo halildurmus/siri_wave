588e39ecbb6c1251cf65f67987d6ee793e282169
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-10-24 00:17:15 +0300

    Use Dark Theme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,9 +8,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: const HomePage(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.amber,
+      ),
+      themeMode: ThemeMode.dark,
     );
   }
 }
