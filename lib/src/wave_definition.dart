1e3833bc1f1a55381b8d2e392fce3de7c266eb59
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-10-23 17:23:27 +0300

    Create WaveDefinition

---
+++ lib/src/wave_definition.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/painting.dart' show Color, Path;
+
+class WaveDefinition {
+  const WaveDefinition({required this.color, required this.path});
+
+  final Color color;
+  final Path path;
+}
