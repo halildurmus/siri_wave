83620e169ee54713f90f6b283545cbc7af2b9985
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-10-23 17:25:22 +0300

    Refactor iOS 13 Siri Wave

--- lib/src/ios_13_siri_wave.dart
+++ lib/src/ios_13_siri_wave.dart
@@ -3,25 +3,46 @@ import 'dart:math' as math;
 import 'package:flutter/material.dart';
 
 import 'painter_controller.dart';
+import 'wave_definition.dart';
 import 'wave_painter.dart';
+
+class _WaveData {
+  int noOfCurves = 0;
+  int spawnAt = 0;
+  double prevMaxY = 0;
+  List<double> phases = [];
+  List<double> offsets = [];
+  List<double> speeds = [];
+  List<double> finalAmplitudes = [];
+  List<double> widths = [];
+  List<double> amplitudes = [];
+  List<double> despawnTimeouts = [];
+  List<double> verses = [];
+}
 
 class IOS13SiriWave extends StatefulWidget {
   const IOS13SiriWave({
     Key? key,
     this.amplitude = 1,
-    required this.color,
     this.speed = .2,
+    this.controller,
   }) : super(key: key);
 
   final double amplitude;
-  final Color color;
   final double speed;
+  final PainterController? controller;
 
   @override
   _IOS13SiriWaveState createState() => _IOS13SiriWaveState();
 }
 
-class _IOS13SiriWaveState extends State<IOS13SiriWave> with SingleTickerProviderStateMixin {
+class _IOS13SiriWaveState extends State<IOS13SiriWave>
+    with SingleTickerProviderStateMixin {
+  static const List<Color> kColors = [
+    Color.fromRGBO(173, 57, 76, 1),
+    Color.fromRGBO(48, 220, 155, 1),
+    Color.fromRGBO(15, 82, 169, 1),
+  ];
   static const double GRAPH_X = 25;
   static const double AMPLITUDE_FACTOR = .8;
   static const int SPEED_FACTOR = 1;
@@ -36,17 +57,11 @@ class _IOS13SiriWaveState extends State<IOS13SiriWave> with SingleTickerProvider
   static const List<double> SPEED_RANGES = [.5, 1];
   static const List<int> DESPAWN_TIMEOUT_RANGES = [500, 2000];
 
-  int _noOfCurves = 0;
-  int _spawnAt = 0;
-  double _prevMaxY = 0;
-  List<double> _phases = [];
-  List<double> _offsets = [];
-  List<double> _speeds = [];
-  List<double> _finalAmplitudes = [];
-  List<double> _widths = [];
-  List<double> _amplitudes = [];
-  List<double> _despawnTimeouts = [];
-  List<double> _verses = [];
+  final Map<String, _WaveData> _wavesData = {
+    'red': _WaveData(),
+    'green': _WaveData(),
+    'blue': _WaveData(),
+  };
 
   late double _height;
   late double _maxHeight;
@@ -56,7 +71,11 @@ class _IOS13SiriWaveState extends State<IOS13SiriWave> with SingleTickerProvider
 
   @override
   void initState() {
-    _painterController = PainterController(widget.color);
+    if (widget.controller == null) {
+      _painterController = PainterController();
+    } else {
+      _painterController = widget.controller!;
+    }
     _animationController = AnimationController(
       vsync: this,
       // Since we don't use AnimationController's value in the animation,
@@ -75,36 +94,39 @@ class _IOS13SiriWaveState extends State<IOS13SiriWave> with SingleTickerProvider
   num _getRandomRange(List<num> e) =>
       e[0] + math.Random().nextDouble() * (e[1] - e[0]);
 
-  void _spawnSingle(int ci) {
-    _phases[ci] = 0;
-    _amplitudes[ci] = 0;
+  void _spawnSingle(int ci, String key) {
+    final wave = _wavesData[key]!;
+    wave.phases[ci] = 0;
+    wave.amplitudes[ci] = 0;
 
-    _despawnTimeouts[ci] = _getRandomRange(DESPAWN_TIMEOUT_RANGES).toDouble();
-    _offsets[ci] = _getRandomRange(OFFSET_RANGES).toDouble();
-    _speeds[ci] = _getRandomRange(SPEED_RANGES).toDouble();
-    _finalAmplitudes[ci] = _getRandomRange(AMPLITUDE_RANGES).toDouble();
-    _widths[ci] = _getRandomRange(WIDTH_RANGES).toDouble();
-    _verses[ci] = _getRandomRange([-1, 1]).toDouble();
+    wave.despawnTimeouts[ci] =
+        _getRandomRange(DESPAWN_TIMEOUT_RANGES).toDouble();
+    wave.offsets[ci] = _getRandomRange(OFFSET_RANGES).toDouble();
+    wave.speeds[ci] = _getRandomRange(SPEED_RANGES).toDouble();
+    wave.finalAmplitudes[ci] = _getRandomRange(AMPLITUDE_RANGES).toDouble();
+    wave.widths[ci] = _getRandomRange(WIDTH_RANGES).toDouble();
+    wave.verses[ci] = _getRandomRange([-1, 1]).toDouble();
   }
 
   List<double> _getEmptyArray(int n) => List.filled(n, 0);
 
-  void _spawn() {
-    _spawnAt = DateTime.now().millisecondsSinceEpoch;
+  void _spawn(String key) {
+    final wave = _wavesData[key]!;
+    wave.spawnAt = DateTime.now().millisecondsSinceEpoch;
 
-    _noOfCurves = _getRandomRange(NOOFCURVES_RANGES).floor();
+    wave.noOfCurves = _getRandomRange(NOOFCURVES_RANGES).floor();
 
-    _phases = _getEmptyArray(_noOfCurves);
-    _offsets = _getEmptyArray(_noOfCurves);
-    _speeds = _getEmptyArray(_noOfCurves);
-    _finalAmplitudes = _getEmptyArray(_noOfCurves);
-    _widths = _getEmptyArray(_noOfCurves);
-    _amplitudes = _getEmptyArray(_noOfCurves);
-    _despawnTimeouts = _getEmptyArray(_noOfCurves);
-    _verses = _getEmptyArray(_noOfCurves);
+    wave.phases = _getEmptyArray(wave.noOfCurves);
+    wave.offsets = _getEmptyArray(wave.noOfCurves);
+    wave.speeds = _getEmptyArray(wave.noOfCurves);
+    wave.finalAmplitudes = _getEmptyArray(wave.noOfCurves);
+    wave.widths = _getEmptyArray(wave.noOfCurves);
+    wave.amplitudes = _getEmptyArray(wave.noOfCurves);
+    wave.despawnTimeouts = _getEmptyArray(wave.noOfCurves);
+    wave.verses = _getEmptyArray(wave.noOfCurves);
 
-    for (int ci = 0; ci < _noOfCurves; ci++) {
-      _spawnSingle(ci);
+    for (int ci = 0; ci < wave.noOfCurves; ci++) {
+      _spawnSingle(ci, key);
     }
   }
 
@@ -113,78 +135,96 @@ class _IOS13SiriWaveState extends State<IOS13SiriWave> with SingleTickerProvider
 
   num _sin(double x, double phase) => math.sin(x - phase);
 
-  num _yRelativePos(double i) {
+  num _yRelativePos(double i, String key) {
+    final wave = _wavesData[key]!;
     double y = 0;
 
-    for (int ci = 0; ci < _noOfCurves; ci++) {
+    for (int ci = 0; ci < wave.noOfCurves; ci++) {
       // Generate a static T so that each curve is distant from each oterh
-      double t = 4 * (-1 + (ci / (_noOfCurves - 1)) * 2);
+      double t = 4 * (-1 + (ci / (wave.noOfCurves - 1)) * 2);
       // but add a dynamic offset
-      t += _offsets[ci];
+      t += wave.offsets[ci];
 
-      final k = 1 / _widths[ci];
+      final k = 1 / wave.widths[ci];
       final x = i * k - t;
 
-      y += (_amplitudes[ci] * _sin(_verses[ci] * x, _phases[ci]) * _globalAttFn(x))
+      y += (wave.amplitudes[ci] *
+              _sin(wave.verses[ci] * x, wave.phases[ci]) *
+              _globalAttFn(x))
           .abs();
     }
 
     // Divide for NoOfCurves so that y <= 1
-    return y / _noOfCurves;
+    return y / wave.noOfCurves;
   }
 
-  double _yPos(double i) => (AMPLITUDE_FACTOR *
+  double _yPos(double i, String key) => (AMPLITUDE_FACTOR *
       _maxHeight *
       widget.amplitude *
-      _yRelativePos(i) *
+      _yRelativePos(i, key) *
       _globalAttFn((i / GRAPH_X) * 2));
 
   double _xPos(double i) => _width * ((i + GRAPH_X) / (GRAPH_X * 2));
 
+  Color _getColor(String key) {
+    if (key == 'red') {
+      return kColors.first;
+    } else if (key == 'green') {
+      return kColors[1];
+    }
+
+    return kColors[2];
+  }
+
   void _calc(PainterController drawingController) {
-    if (_spawnAt == 0) {
-      _spawn();
-    }
-
-    for (int ci = 0; ci < _noOfCurves; ci++) {
-      if (_spawnAt + _despawnTimeouts[ci] <=
-          DateTime.now().millisecondsSinceEpoch) {
-        _amplitudes[ci] -= DESPAWN_FACTOR;
-      } else {
-        _amplitudes[ci] += DESPAWN_FACTOR;
+    for (final entry in _wavesData.entries) {
+      final wave = entry.value;
+      final color = _getColor(entry.key);
+      if (wave.spawnAt == 0) {
+        _spawn(entry.key);
       }
 
-      _amplitudes[ci] =
-          math.min(math.max(_amplitudes[ci], 0), _finalAmplitudes[ci]);
-      _phases[ci] =
-          (_phases[ci] + widget.speed * _speeds[ci] * SPEED_FACTOR) % (2 * math.pi);
-    }
+      for (int ci = 0; ci < wave.noOfCurves; ci++) {
+        if (wave.spawnAt + wave.despawnTimeouts[ci] <=
+            DateTime.now().millisecondsSinceEpoch) {
+          wave.amplitudes[ci] -= DESPAWN_FACTOR;
+        } else {
+          wave.amplitudes[ci] += DESPAWN_FACTOR;
+        }
 
-    double maxY = double.negativeInfinity;
-    double minX = double.infinity;
-
-    // Write two opposite waves
-    for (final sign in [1, -1]) {
-      final path = Path();
-      path.moveTo(0, _maxHeight);
-      for (double i = -GRAPH_X; i <= GRAPH_X; i += PIXEL_DEPTH) {
-        final x = _xPos(i);
-        final y = _yPos(i);
-        path.lineTo(x, _maxHeight - sign * y);
-
-        minX = math.min(minX, x);
-        maxY = math.max(maxY, y);
+        wave.amplitudes[ci] = math.min(
+            math.max(wave.amplitudes[ci], 0), wave.finalAmplitudes[ci]);
+        wave.phases[ci] =
+            (wave.phases[ci] + widget.speed * wave.speeds[ci] * SPEED_FACTOR) %
+                (2 * math.pi);
       }
 
-      path.close();
-      drawingController.add(path);
-    }
+      double maxY = double.negativeInfinity;
+      double minX = double.infinity;
 
-    if (maxY < DEAD_PX && _prevMaxY > maxY) {
-      _spawnAt = 0;
-    }
+      // Write two opposite waves
+      for (final sign in [1, -1]) {
+        final path = Path();
+        path.moveTo(0, _maxHeight);
+        for (double i = -GRAPH_X; i <= GRAPH_X; i += PIXEL_DEPTH) {
+          final x = _xPos(i);
+          final y = _yPos(i, entry.key);
+          path.lineTo(x, _maxHeight - sign * y);
 
-    _prevMaxY = maxY;
+          minX = math.min(minX, x);
+          maxY = math.max(maxY, y);
+        }
+
+        path.close();
+        drawingController.add(WaveDefinition(color: color, path: path));
+      }
+
+      if (maxY < DEAD_PX && wave.prevMaxY > maxY) {
+        wave.spawnAt = 0;
+      }
+
+      wave.prevMaxY = maxY;
+    }
   }
 
   @override
@@ -194,13 +234,13 @@ class _IOS13SiriWaveState extends State<IOS13SiriWave> with SingleTickerProvider
         _height = constraints.maxHeight;
         _maxHeight = _height / 2;
         _width = constraints.maxWidth;
+        final painter = WavePainter(_painterController, _maxHeight, _width);
+
         return AnimatedBuilder(
           animation: _animationController,
           builder: (_, __) {
             _calc(_painterController);
-            return CustomPaint(
-              painter: WavePainter(_painterController, _maxHeight, _width),
-            );
+            return CustomPaint(painter: painter);
           },
         );
       },
