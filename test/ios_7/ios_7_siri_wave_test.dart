0800d05eb43bfe2352ed66cd736cf290f0033a7c
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-10-29 00:11:09 +0300

    Add widget tests for IOS7SiriWave

---
+++ test/ios_7/ios_7_siri_wave_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:siri_wave/siri_wave.dart';
+import 'package:siri_wave/src/ios_7/ios_7_siri_wave.dart';
+import 'package:siri_wave/src/ios_7/ios_7_siri_wave_painter.dart';
+
+void main() {
+  group('IOS7SiriWave', () {
+    testWidgets('widget\'s properties should be set correctly',
+        (WidgetTester tester) async {
+      // Build the SiriWave widget.
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Material(
+            child: IOS7SiriWave(options: IOS7Options()),
+          ),
+        ),
+      );
+      // Find the IOS7SiriWave widget.
+      final ios7SiriWave =
+          tester.firstWidget<IOS7SiriWave>(find.byType(IOS7SiriWave));
+      expect(ios7SiriWave.frequency, 6);
+      expect(ios7SiriWave.options.amplitude, 1);
+      expect(ios7SiriWave.speed, .2);
+    });
+
+    testWidgets('widget should paint the canvas with IOS7SiriWavePainter',
+        (WidgetTester tester) async {
+      // Build the SiriWave widget.
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Material(
+            child: IOS7SiriWave(
+              options: IOS7Options(),
+            ),
+          ),
+        ),
+      );
+      // Find the CustomPaint widget.
+      final customPaint = tester.firstWidget<CustomPaint>(
+        find.descendant(
+          of: find.byType(AnimatedBuilder),
+          matching: find.byType(CustomPaint),
+        ),
+      );
+      expect(customPaint.painter, isA<IOS7SiriWavePainter>());
+    });
+  });
+}
