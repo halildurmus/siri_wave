9fcc3cd172b9dbd923467c35a10d07f646127cea
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-11-02 18:32:16 +0300

    Update example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:siri_wave/siri_wave.dart';
 
 void main() => runApp(const MyApp());
@@ -31,39 +32,140 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   double _amplitude = 1;
+  double _frequency = 6;
   double _speed = .2;
   final _isSelected = [false, true];
-  final SiriWaveController _controller = SiriWaveController();
+  final _controller = SiriWaveController();
+  Color _currentColor = Colors.white;
+
+  void _changeColor(Color color) {
+    _currentColor = color;
+    _controller.setColor(color);
+  }
 
   Widget _buildAmplitudeSlider() {
-    return SizedBox(
-      width: 360,
-      child: Slider(
-        value: _amplitude,
-        min: 0,
-        max: 1,
-        onChanged: (double value) {
-          _controller.setAmplitude(value);
-          _amplitude = value;
-          setState(() {});
-        },
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: SizedBox(
+        width: 360,
+        child: Slider(
+          value: _amplitude,
+          min: 0,
+          max: 1,
+          onChanged: (double value) {
+            _controller.setAmplitude(value);
+            _amplitude = value;
+            setState(() {});
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFrequencySlider() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: SizedBox(
+        width: 360,
+        child: Slider(
+          value: _frequency,
+          divisions: 40,
+          min: -20,
+          max: 20,
+          onChanged: (double value) {
+            _controller.setFrequency(value.round());
+            _frequency = value;
+            setState(() {});
+          },
+        ),
       ),
     );
   }
 
   Widget _buildSpeedSlider() {
-    return SizedBox(
-      width: 360,
-      child: Slider(
-        value: _speed,
-        min: 0,
-        max: 1,
-        onChanged: (double value) {
-          _controller.setSpeed(value);
-          _speed = value;
-          setState(() {});
-        },
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: SizedBox(
+        width: 360,
+        child: Slider(
+          value: _speed,
+          min: 0,
+          max: 1,
+          onChanged: (double value) {
+            _controller.setSpeed(value);
+            _speed = value;
+            setState(() {});
+          },
+        ),
       ),
+    );
+  }
+
+  Widget _buildFrequencySection() {
+    return AnimatedSize(
+      curve: Curves.fastOutSlowIn,
+      duration: const Duration(milliseconds: 400),
+      child: _isSelected[0]
+          ? Column(
+              children: [
+                Text('Frequency', style: Theme.of(context).textTheme.headline6),
+                _buildFrequencySlider(),
+              ],
+            )
+          : const SizedBox(),
+    );
+  }
+
+  Widget _buildWaveColorSection() {
+    void _showColorPickerDialog() {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            titlePadding: const EdgeInsets.all(0.0),
+            contentPadding: const EdgeInsets.all(0.0),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(9),
+              ),
+            ),
+            content: SingleChildScrollView(
+              child: ColorPicker(
+                pickerColor: _currentColor,
+                onColorChanged: _changeColor,
+                pickerAreaHeightPercent: .7,
+                displayThumbColor: true,
+                paletteType: PaletteType.hsl,
+                pickerAreaBorderRadius: const BorderRadius.all(
+                  Radius.circular(8),
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    }
+
+    return AnimatedSize(
+      curve: Curves.fastOutSlowIn,
+      duration: const Duration(milliseconds: 400),
+      child: _isSelected[0]
+          ? Column(
+              children: [
+                Text(
+                  'Wave Color',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: ElevatedButton(
+                    onPressed: () => _showColorPickerDialog(),
+                    child: const Text('Change color'),
+                  ),
+                ),
+              ],
+            )
+          : const SizedBox(),
     );
   }
 
@@ -74,11 +176,10 @@ class _HomePageState extends State<HomePage> {
           return;
         }
 
-        setState(() {
-          for (int i = 0; i < _isSelected.length; i++) {
-            _isSelected[i] = i == index;
-          }
-        });
+        for (int i = 0; i < _isSelected.length; i++) {
+          _isSelected[i] = i == index;
+        }
+        setState(() {});
       },
       isSelected: _isSelected,
       borderColor: Theme.of(context).primaryColorLight,
@@ -102,13 +203,19 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _buildSiriWave() {
-    return SiriWave(
-      controller: _controller,
-      options: const SiriWaveOptions(
-        height: kIsWeb ? 300 : 180,
-        width: kIsWeb ? 600 : 360,
-      ),
-      style: _isSelected[0] ? SiriWaveStyle.ios_7 : SiriWaveStyle.ios_9,
+    return Column(
+      children: [
+        _buildDivider(),
+        SiriWave(
+          controller: _controller,
+          options: const SiriWaveOptions(
+            height: kIsWeb ? 300 : 180,
+            width: kIsWeb ? 600 : 360,
+          ),
+          style: _isSelected[0] ? SiriWaveStyle.ios_7 : SiriWaveStyle.ios_9,
+        ),
+        _buildDivider(),
+      ],
     );
   }
 
@@ -117,22 +224,18 @@ class _HomePageState extends State<HomePage> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          if (!kIsWeb) const Spacer(),
+          const Spacer(),
           Text('Amplitude', style: Theme.of(context).textTheme.headline6),
-          const SizedBox(height: 10),
           _buildAmplitudeSlider(),
-          const SizedBox(height: 10),
           Text('Speed', style: Theme.of(context).textTheme.headline6),
-          const SizedBox(height: 10),
           _buildSpeedSlider(),
-          const SizedBox(height: 10),
+          _buildFrequencySection(),
+          _buildWaveColorSection(),
           Text('Style', style: Theme.of(context).textTheme.headline6),
-          const SizedBox(height: 30),
+          const SizedBox(height: 15),
           _buildToggleButtons(),
-          if (kIsWeb) const SizedBox(height: 150) else const Spacer(),
-          _buildDivider(),
+          const Spacer(),
           _buildSiriWave(),
-          _buildDivider(),
         ],
       ),
     );
